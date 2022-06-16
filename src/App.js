6b64f45cf643073ef3912fed1c031f4970653aa7
David <david.jesus.sebastian.flores@gmail.com>
2022-06-15 22:23:53 -0300

    routes pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import Detail from "./pages/Detail/Detail";
+import Home from "./pages/Home/Home";
+import Landing from "./pages/Landing/Landing";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Terms from "./pages/Terms/Terms";
+import Error from "./pages/Error/Error";
 
 const App = () => {
   return (
     <>
-      <h1>CineMagic</h1>
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/terms' element={<Terms />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/detail' element={<Detail />} />
+        <Route path='/*' element={<Error />} />
+      </Routes>
     </>
   );
 }

9c5d406af4a4d8396bfb96dab59a65beb161379e
monicabatallanes <mbbatallanes1@gmail.com>
2022-06-22 18:01:13 -0300

    agregué pagina error

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { Route, Routes } from "react-router-dom";
-import Detail from './pages/Detail/Detail'
-import Home from './Pages/Home/Home'
-import Landing from './Pages/Landing/Landing'
-import Login from './Pages/Login/Login'
-import Register from './Pages/Register/Register'
-import Terms from './Pages/Terms/Terms'
-import Error from './Pages/Error/Error'
-import Admin from './Pages/Admin/Admin'
+import Detail from "./pages/Detail/Detail";
+import Home from "./Pages/Home/Home";
+import Landing from "./Pages/Landing/Landing";
+import Login from "./Pages/Login/Login";
+import Register from "./Pages/Register/Register";
+import Terms from "./Pages/Terms/Terms";
+import Error from "./Pages/Error/Error";
+import Admin from "./Pages/Admin/Admin";
 
 const App = () => {
   return (
--- src/pages/Error/Error.js
+++ src/pages/Error/Error.js
@@ -1,7 +1,20 @@
+import React from "react";
+import error404 from "../../assets/images/image-error404.jpg"
+
 const Error = () => {
   return ( 
-    <h1>Error Page</h1>
+    
+            <div className="container">
+                <img className=" img-fluid" src={error404} alt="imagen-error-404"/>
+                <h6 className="text-white text-center m-2">
+                 La página solicitada no está disponible
+                </h6>
+                <div className="button d-flex justify-content-center">
+                <button className="btn btn-danger m-2 p-1"><a href="/" class="btn text-white"> Volver </a> </button>
+                </div>
+            </div>
+        
   );
-}
+};
 
 export default Error;

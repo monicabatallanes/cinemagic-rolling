59fa88f64fc1d272748b1b9c43a06d79e1b9e6c8
Nahuel de la Torre <nahueldelatorre16@gmail.com>
2022-06-23 18:07:39 -0300

    PlanMarketing

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,12 +1,14 @@
 import Hero from "../../components/Hero/Hero";
 import Navbar from "../../components/Navbar/Navbar";
+import PlanMarketing from "../../components/PlanMarketing/PlanMarketing";
 
 const Landing = () => {
   return ( 
     <div>
        <Navbar />
        <div>
-         <Hero /> 
+         <Hero />
+         <PlanMarketing /> 
        </div>
     </div>
   );

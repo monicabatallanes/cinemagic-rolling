c19c3ce9d8fdab71e4f7ab2bf389ce22c65d6a3e
Nahuel de la Torre <nahueldelatorre16@gmail.com>
2022-06-24 21:42:48 -0300

    creé el componente slider

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -2,9 +2,10 @@ import DispositivosAdaptables from "../../components/DispositivosAdaptables/Disp
 import Hero from "../../components/Hero/Hero";
 import MenuNavegacion from "../../components/MenuNavegacion/MenuNavegacion";
 import PlanMarketing from "../../components/PlanMarketing/PlanMarketing";
+import Slider from "../../components/Slider/Slider";
 
 const Landing = () => {
-  return ( 
+  return (
     <div>
       <header>
         <MenuNavegacion />
@@ -15,8 +16,20 @@ const Landing = () => {
           <PlanMarketing />
         </section>
         <section>
+          <Slider
+            video={"https://youtu.be/0kna4HKOi5E"}
+            titulo={"El Padre De La Novia"}
+            descripcion={"Disfruta del amor, la comida, la música y las risas de esta historia de una familia de fuertes que es capaz de adaptarse a lo imposible por amor."}
+          />
+          <Slider
+            video={"https://youtu.be/-wHuLZ9ssg0"}
+            titulo={"Animales Fantásticos los Secretos de Dumbledore"}
+            descripcion={"Dumbledore sabe que el poderoso mago oscuro Gellert Grindelwald se está moviendo para tomar el control del mundo mágico, ¿lo logrará?"}
+          />
+        </section>
+        <section>
           <DispositivosAdaptables />
-        </section>  
+        </section>
       </main>
     </div>
   );

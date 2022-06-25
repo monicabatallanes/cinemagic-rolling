d7cdfaeb460472196f1f4e8b65bb9b3eb4ccc7a1
David <david.jesus.sebastian.flores@gmail.com>
2022-06-25 20:37:13 -0300

    api the movie db

--- src/components/MovieDashboard/MovieDashboard.js
+++ src/components/MovieDashboard/MovieDashboard.js
@@ -1,4 +1,8 @@
 import { useForm } from 'react-hook-form';
+import { useState } from "react";
+import "../MovieDashboard/movieDashboard.css"
+import iconSearch from '../../assets/images/search.svg';
+import iconClose from '../../assets/images/close.svg';
 
 const MovieDashboard = (props) => {
   const urlBackend = process.env.REACT_APP_URL; 
@@ -6,7 +10,7 @@ const MovieDashboard = (props) => {
   const cambiaEstadoEditar = props.cambiaEstadoEditar;
   const cambiaEstadoAgregar = props.cambiaEstadoAgregar;
   const titulo=agregar? ("Agregar Pelicula") : (editar? "Editar Pelicula": "E  R  R  O  R");
-  const { register, handleSubmit, formState: { errors } } = useForm();
+  const { reset, register, handleSubmit, formState: { errors } } = useForm();
 
   const onSubmit = async(data) => {
     const userJson = localStorage.getItem('cinemagicUser');
@@ -51,7 +55,6 @@ const MovieDashboard = (props) => {
   } 
 
   const deleteData = async() => {
-    
     const uid= objeto._id;
     const data = {
       uid : uid,
@@ -85,9 +88,112 @@ const MovieDashboard = (props) => {
     }
   }
 
+  const [movies, setMovies] = useState([]);
+  const [text, setText] = useState("");
+  const [id, setId] = useState(0);
+  const [suggestions, setSuggestions] = useState([]);
+  const urlImagenes = "https://image.tmdb.org/t/p/original";
+
+  const loadMovies = async (data) => {
+    const resp = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=4d364ad85512cc46ece523adfe038aba&query=${data}&page=1`)
+    const json = await resp.json()
+    setMovies(json.results);
+  }
+
+  const loadSelectedMovie = async (data) => {
+    const response = await fetch(`https://api.themoviedb.org/3/movie/${data}?api_key=4d364ad85512cc46ece523adfe038aba&language=en-US`)
+    const resultJson = await response.json()
+
+    let defaultValues = {};
+    defaultValues.titulo = resultJson.original_title;
+    defaultValues.sinopsis = resultJson.overview;
+    defaultValues.lanzamiento = resultJson.release_date;
+    defaultValues.poster = urlImagenes + resultJson.poster_path;
+    defaultValues.fondo = urlImagenes + resultJson.backdrop_path;
+    reset({ ...defaultValues });
+  }
+
+  const onChangeHandler = (text) => {
+    let matches = [];
+    if (text.length > 3) {
+      // busco el texto en la api de busqueda
+      loadMovies(text);
+      // filtro
+      matches = movies.filter((peli) => {
+        const regex = new RegExp(`${text}`, "gi");
+        return peli.original_title.match(regex);
+      });
+    }
+    setSuggestions(matches);
+    setText(text);
+  };
+
+  const onSuggestHandler = (text, id) => {
+    setText(text);
+    setId(id);
+    setSuggestions([]);
+    
+  };
+
+  const clearInput = () => {
+    setSuggestions([]);
+    setText("");
+  };
+
+  const fillFields = () => {
+    if (text.length > 3) {
+      // busco el texto en la api de detalles 
+      loadSelectedMovie(id);
+    }
+  };
+
   return ( 
     <>
-      <div className="col p-5 caja-login ">
+      {
+        agregar? (
+          <div className="container search">
+            <div className="searchInputs">
+              <input
+                type="text"
+                className="form-control entrada-login" 
+                onChange={(e) => onChangeHandler(e.target.value)}
+                value={text}
+                placeholder="Ingrese el nombre de la pelicula"
+                onBlur={() => {
+                  setTimeout(() => {
+                    setSuggestions([]);
+                  }, 100);
+                }}
+              />
+              <div className="searchIcon">
+                {suggestions.length === 0 ? (
+                  <button className="boton-search-close">
+                    <img src={iconSearch} onClick={fillFields} />
+                  </button>
+                ) : (
+                  <button className="boton-search-close" id="clearBtn" onClick={clearInput}>
+                    <img src={iconClose} />
+                  </button>
+                )}
+              </div>
+            </div>
+            {suggestions &&
+              suggestions.map((suggestion, i) => (
+                <div
+                  key={i}
+                  className="suggestions "
+                  onClick={() => onSuggestHandler(suggestion.original_title, suggestion.id)}
+                >
+                  {suggestion.original_title}
+                </div>
+              ))}
+          </div>
+        ) : (
+          <br></br>
+        )
+      }
+      
+      <div className="col p-5 caja-login mt-5">
         <h2 className="fw-bold text-center pt-2 mb-4 ">{titulo}</h2>
         <form className="row" onSubmit={handleSubmit(onSubmit)}>
           <div className="has-validation mb-4">
@@ -202,7 +308,7 @@ const MovieDashboard = (props) => {
                 <div className="d-grid gap-2 col-12">
                   <button type="submit" className="btn boton-login" >Actualizar</button>
                 </div>
-            ) 
+              ) 
             }
           </div>
         </form>
